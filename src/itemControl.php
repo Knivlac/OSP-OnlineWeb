8d0c0a5f1e09c2eb3507414c8cdd894ae7e59eca
Viljo <126386178+Viljo-Valerian@users.noreply.github.com>
2023-05-30 14:13:37 +0800

    Add files via upload

---
+++ src/itemControl.php
@@ -0,0 +1,34 @@
+<?php 
+error_reporting(E_ALL ^ E_WARNING); 
+
+$con = mysqli_connect("localhost","root","","bookzone");
+
+$ISBN = $_GET['ISBN'];
+$title = $_GET['title'];
+$author = $_GET['author'];
+$cover = $_GET['cover'];
+$price = $_GET['price'];
+$genre = $_GET['genre'];
+$description = $_GET['description'];
+$cmd = $_GET['cmd'];
+
+if($cmd =="submit"){
+	if(empty($ISBN && $title && $author && $cover && $price && $genre && $description)){
+		die("Please do not leave anything empty");
+	}else{
+		$sql = "INSERT INTO booknew(ISBN,title,author,cover,price,genre,description) VALUES('$ISBN','$title','$author','$cover','$price','$genre','$description')";
+
+		mysqli_query($con,$sql);	
+	}
+}else if($cmd == "edit"){
+		$sql = "UPDATE booknew SET title='$title', author='$author', cover='$cover', price='$price', genre='$genre', description='$description' WHERE ISBN= '$ISBN'";
+		
+		mysqli_query($con,$sql);
+
+}else if ($cmd == "delete") {
+	$sql = "DELETE FROM booknew WHERE ISBN='$ISBN'";
+	
+	header("location: itemDisplay.php");
+	mysqli_query($con,$sql);
+}
+?>
---
+++ src/itemDisplay.php
@@ -0,0 +1,108 @@
+<?php include 'itemControl.php';
+error_reporting(E_ALL ^ E_WARNING); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="Stylesheet" href="admin_the_style.css" 
+	/>
+    <link
+        rel="Stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+	<title>Inventory Management</title>
+	<script>
+    	function cmd_edit(ISBN,title,author,cover,price,genre,description){
+    		document.getElementById('ISBN').value = ISBN;
+    		document.getElementById('title').value = title;
+    		document.getElementById('author').value = author;
+    		document.getElementById('price').value = price;
+    		document.getElementById('genre').value = genre;
+    		document.getElementById('description').value = description;
+			document.getElementById('cover').value = cover;
+    		document.getElementById('cmd').value = "edit";
+    	}
+		function cmd_del(ISBN){
+			document.getElementById('ISBN').value = ISBN;
+			window.location.href = "itemControl.php?cmd=delete&ISBN=" + ISBN;
+		}
+	</script>
+</head>
+<body>
+	<div class="banner">
+      	<a href="../main.html"><img class="logo" src="../graphic/Sprite-Logo.png"/></a>
+    </div>
+
+	<div class="input-div">
+    	<h2 style="color: white">Input/Edit Item Data</h2>
+		<br>
+		<form class="input-form" method="GET" autocomplete="off">
+			<label>ISBN</label><input type="text" name="ISBN" id="ISBN">
+			<label>Title</label><input type="text" name="title" id="title">
+			<label>Author</label><input type="text" name="author" id="author">
+			<label>Price</label><input type="number" name="price" id="price" step="any">
+			<label>Genre</label><input type="text" name="genre" id="genre">
+			<br>
+			<div class="tall-guys">
+				<label>Description</label><textarea type="text" name="description" id="description"></textarea>
+				<label>Cover</label><input type="file" name="cover" id="cover">
+			</div>
+			<input type="submit" id="cmd" name="cmd" value="submit">
+		</form>
+	</div>
+	
+	<div class="display-list">
+    	<h2>Currently In Display</h2>
+    	<table>
+    		<thead>
+	    		<tr>
+	    			<th>ISBN</th>
+	    			<th>Title</th>
+	    			<th>Author</th>
+	    			<th>Price</th>
+	    			<th>Genre</th>
+	    			<th class="display-desc">Description</th>
+					<th>Cover</th>
+	    			<th class="display-sakanaction" colspan="2">Action</th>
+	    		</tr>
+    		</thead>
+    		<tbody>
+    			<?php 
+    				$sql = "SELECT * FROM booknew";
+    				$query = mysqli_query($con,$sql);
+    				$row = mysqli_num_rows($query);
+
+    				for ($i=0; $i < $row; $i++) { 
+    					$result = mysqli_fetch_array($query);
+    					$ISBN = $result['ISBN'];
+						$title = $result['title'];
+						$author = $result['author'];
+						$price = $result['price'];
+						$genre = $result['genre'];
+						$description = $result['description'];
+						$cover = $result['cover'];
+				?>
+				<tr>
+					<td><?php echo $ISBN ?></td>
+					<td><?php echo $title ?></td>
+					<td><?php echo $author ?></td>
+					<td><?php echo $price ?></td>
+					<td><?php echo $genre ?></td>
+					<td class="display-desc"><?php echo $description ?></td>
+					<td><img src="graphic/<?php echo $cover ?>" style="width: 100px; height: 100px;"></td>
+					<td>
+					<input type="submit" name="cmd" id="cmd_del" value="delete" onclick="cmd_del(<?php echo "'$ISBN'"?>)">
+					</td>
+					<td>
+					<input type="submit" name="cmd" id="cmd_edit" value="edit" onclick="cmd_edit(<?php echo "'$ISBN','$title','$author','$cover','$price','$genre','$description'"?>)">
+					</td>
+				</tr>
+    			<?php } ?>
+    		</tbody>
+    	</table>
+    	<a class="adminLogin" href="adminControl.html">Back</a>
+	</div>
+</body>
+</html>
