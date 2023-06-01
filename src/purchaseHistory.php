1a25971075251f75d61dc70099cd7553bc3524da
PekoMarisa <floweychanisthebest@gmail.com>
2023-06-01 16:29:53 +0800

    Update purchaseHistory.php

--- src/purchaseHistory.php
+++ src/purchaseHistory.php
@@ -2,4 +2,19 @@
 error_reporting(E_ALL ^ E_WARNING); 
 
 $con = mysqli_connect("localhost","root","","bookzone");
+
+$ISBN = $_GET['ISBN'];
+$title = $_GET['title'];
+$author = $_GET['author'];
+$cover = $_GET['cover'];
+$price = $_GET['price'];
+$genre = $_GET['genre'];
+$description = $_GET['description'];
+$cmd = $_GET['cmd'];
+
+if($cmd =="submit"){
+		$sql = "INSERT INTO booknew(ISBN,title,author,cover,price,genre,description) VALUES('$ISBN','$title','$author','$cover','$price','$genre','$description')";
+
+		mysqli_query($con,$sql);
+}
 ?>
